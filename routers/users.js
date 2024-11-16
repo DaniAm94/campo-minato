bed48d63b3ca0352b1f91cad566e99b44648606e
DaniAm94 <dani.amico94@gmail.com>
2024-11-16 19:07:02 +0100

    added request validation to users' router

--- routers/users.js
+++ routers/users.js
@@ -4,11 +4,15 @@ const router = express.Router();
 
 const users = require("../controllers/users.js");
 
+const validator = require("../middlewares/validator.js");
+
+const { registerBody, loginBody } = require("../validations/users.js");
+
 
 // Rotte
 
-router.post('/register', users.register);
+router.post('/register', validator(registerBody), users.register);  // registrazione
 
-router.post('/login', users.login);
+router.post('/login', validator(loginBody), users.login);   // login
 
 module.exports = router;
