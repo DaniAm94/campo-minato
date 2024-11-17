41419fd9015f8f537bad9c4effe4ce98519ad13f
DaniAm94 <dani.amico94@gmail.com>
2024-11-17 21:14:20 +0100

    created a middleware to validate slug from request's params

---
+++ middlewares/validateSlug.js
@@ -0,0 +1,17 @@
+// Middleware per la validazione dello slug utente
+
+module.exports = (req, res, next) => {
+
+    // Preleva i dati dell'utente attualmente loggato
+    const currentUser = req.user;
+
+    // Preleva lo slug dalla request
+    const slug = req.params.slug;
+
+    // Se lo slug dell'utente loggato non coincide con quello nella request
+    if (currentUser.slug !== slug) {
+        // Viene passato un errore a next che verrà quindi catturato dal middleware errorHandler
+        return next(new Error('Non sei autorizzato', 403));
+    }
+    next();
+}
