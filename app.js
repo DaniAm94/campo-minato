479648133ba9287094509660ca0961cb516b7da7
DaniAm94 <dani.amico94@gmail.com>
2024-11-17 21:16:20 +0100

    added errorHandler middleware into the main server

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ const app = express();
 //I middleware
 
 const notFound = require("./middlewares/notFound.js");
+const errorHandler = require("./middlewares/errorHandler.js");
 
 //I router
 const users = require("./routers/users.js");
 
 app.use(express.json());
 
-
 app.use('/users', users);
 
-
 app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server attivo su http://localhost:${port}`);
