2f6b805e20b982caa4bee12d06b3409b565e864b
DaniAm94 <dani.amico94@gmail.com>
2024-11-17 21:15:52 +0100

    enabled users' router to handle games routes

--- routers/users.js
+++ routers/users.js
@@ -4,9 +4,14 @@ const router = express.Router();
 
 const users = require("../controllers/users.js");
 
+const games = require("./games.js");
+
 const validator = require("../middlewares/validator.js");
 
 const { registerBody, loginBody } = require("../validations/users.js");
+
+const auth = require("../middlewares/auth.js");
+const validateSlug = require("../middlewares/validateSlug.js");
 
 
 // Rotte
@@ -14,5 +19,10 @@ const { registerBody, loginBody } = require("../validations/users.js");
 router.post('/register', validator(registerBody), users.register);  // registrazione
 
 router.post('/login', validator(loginBody), users.login);   // login
+
+router.use(auth);
+router.use('/:slug', validateSlug);
+
+router.use('/:slug/games', games)
 
 module.exports = router;
