49d34d5bd79c6e5c91e5712d4462340890dd2bda
DaniAm94 <dani.amico94@gmail.com>
2024-11-19 16:25:36 +0100

    added function to get all the games related to the current user

--- controllers/games.js
+++ controllers/games.js
@@ -1,5 +1,30 @@
+const { PrismaClient } = require("@prisma/client");
+const errorHandlerFunction = require("../utils/errorHandlerFunction.js");
+const prisma = new PrismaClient();
+
 module.exports = {
-    create: (req, res) => {
+    index: async (req, res) => {
+
+        // Prelevo l'id dell'utente dalla request
+        userId = req.user.id;
+        try {
+
+            // Prendo tutte le partite che hanno un match con lo userId
+            const games = await prisma.game.findMany({
+                where: { userId },
+                orderBy: {
+                    startTime: 'desc'
+                }
+            });
+
+            res.status(200).json(games);
+
+        } catch (err) {
+            errorHandlerFunction(res, err)
+        }
+
+    },
+    store: (req, res) => {
         return res.json("Crea una nuova partita");
     }
 }
