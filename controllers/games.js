923fc18798a3bc974c07013184479cbc386038d5
DaniAm94 <dani.amico94@gmail.com>
2024-11-19 21:49:16 +0100

    completed game store

--- controllers/games.js
+++ controllers/games.js
@@ -1,12 +1,14 @@
 const { PrismaClient } = require("@prisma/client");
 const errorHandlerFunction = require("../utils/errorHandlerFunction.js");
+const generateGrid = require("../utils/generateGrid.js");
+const generateCells = require("../utils/generateCells.js");
 const prisma = new PrismaClient();
 
 module.exports = {
     index: async (req, res) => {
 
         // Prelevo l'id dell'utente dalla request
-        userId = req.user.id;
+        const userId = req.user.id;
         try {
 
             // Prendo tutte le partite che hanno un match con lo userId
@@ -24,7 +26,50 @@ module.exports = {
         }
 
     },
-    store: (req, res) => {
-        return res.json("Crea una nuova partita");
+    store: async (req, res) => {
+
+        const { difficulty } = req.body;
+        const userId = req.user.id;
+        const gameData = {
+            difficulty,
+            userId
+        }
+        try {
+
+            // Rendo la creazione di game, grid e cells un'operazione atomica. Se una fallisce, falliscono tutte
+            const { game, grid } = await prisma.$transaction(async (prisma) => {
+
+                // Creazione partita
+                const game = await prisma.game.create({ data: gameData })
+
+                //Configurazione griglia
+                const gridData = generateGrid(game.id, difficulty);
+                // Creazione griglia della partita
+                const grid = await prisma.grid.create({ data: gridData })
+
+                // Aggiorno il campo gridId del game
+                await prisma.game.update({
+                    where: { id: game.id },
+                    data: { gridId: grid.id }
+                });
+
+                // Creazione celle
+                const cells = generateCells(grid.id, grid.height, grid.width, grid.mineCount);
+                // Salvataggio celle nel database
+                await prisma.cell.createMany({ data: cells, skipDuplicates: true });
+
+                return { game, grid };
+            })
+
+            return res.status(201).json({
+                message: 'Partita creata con successo!',
+                game: { ...game, gridId: grid.id },
+                grid,
+            });
+
+        } catch (err) {
+            errorHandlerFunction(res, err)
+        }
+
     }
 }
