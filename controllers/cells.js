72f24628320b8e149d65c8c282dbcbb150f183e0
DaniAm94 <dani.amico94@gmail.com>
2024-11-22 21:09:01 +0100

    added cells' controller response

--- controllers/cells.js
+++ controllers/cells.js
@@ -1,4 +1,8 @@
 module.exports = {
-    reveal: async (req, res) => { },
-    flag: async (req, res) => { }
+    reveal: async (req, res) => {
+        res.status(200).json("Sono reveal cells")
+    },
+    flag: async (req, res) => {
+        res.status(200).json("Sono flag cells")
+    }
 }
