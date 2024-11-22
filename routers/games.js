5881dc82d0fe17eb6abbeac878c814888f59dcb9
DaniAm94 <dani.amico94@gmail.com>
2024-11-22 19:59:05 +0100

    added routes to updated games' status and to handle cells' action

--- routers/games.js
+++ routers/games.js
@@ -4,17 +4,24 @@ const router = express.Router();
 
 const games = require("../controllers/games.js");
 
+const cells = require("./cells.js");
+
 const validator = require("../middlewares/validator.js");
 
 // Rotte
 
 router.get('/', games.index); // lista partite
 
-router.get('/:id', games.show); // restituisce i dettagli di una partita
-
 router.post('/', games.store);  // crea una partita
 
+router.get('/:id', games.show); // restituisce i dettagli di una partita
+
+router.patch('/:id', games.updateStatus); // aggiorna lo status di una partita
+
 router.get('/:id/in-progress', games.resume); // riprende una partita in corso
+
+router.use('/:id/cells', cells) // rotte relative alle celle
+
 
 
 
