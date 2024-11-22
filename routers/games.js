e915ce8121274a9db04c86f3264a8515817b7de0
DaniAm94 <dani.amico94@gmail.com>
2024-11-22 21:30:25 +0100

    created a route to delete a game from db

--- routers/games.js
+++ routers/games.js
@@ -18,6 +18,8 @@ router.get('/:gameId', games.show); // restituisce i dettagli di una partita
 
 router.patch('/:gameId', games.updateStatus); // aggiorna lo status di una partita
 
+router.delete('/:gameId', games.destroy) // cancella una partita
+
 router.get('/:gameId/in-progress', games.resume); // riprende una partita in corso
 
 router.use('/:gameId/cells', cells) // rotte relative alle celle
