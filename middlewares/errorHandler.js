b60527f907ba72eda1f7d721b67e25790f1f0d86
DaniAm94 <dani.amico94@gmail.com>
2024-11-23 18:36:41 +0100

    removed useless console log

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,6 +2,5 @@ module.exports = (err, req, res, next) => {
     console.error(err);
     const statusCode = err.statusCode || 500;
     const message = err.customMessage || 'Server Error';
-    console.log(err);
     res.status(statusCode).json({ error: message });
 }
