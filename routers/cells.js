2c8e2858fbe72e595298ab210d253c932f3353a5
DaniAm94 <dani.amico94@gmail.com>
2024-11-23 17:28:14 +0100

    added middleware to check cells' revealed attribute into cells' router

--- routers/cells.js
+++ routers/cells.js
@@ -11,13 +11,16 @@ const validator = require("../middlewares/validator.js");
 // Regole di validazione
 const { paramsId } = require("../validations/cells.js");
 
+// Middleware che controlla le celle
+const checkCellRevealed = require("../middlewares/checkCellRevealed.js");
+
 
 
 // Rotte
 
 router.patch('/:cellId/reveal', validator(paramsId), cells.reveal); // rivela la cella
 
-router.patch('/:cellId/flag', validator(paramsId), cells.flag); // flagga la cella
+router.patch('/:cellId/flag', [validator(paramsId), checkCellRevealed], cells.flag); // flagga la cella
 
 
 module.exports = router;
