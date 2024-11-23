e2d92dee0cf4664792cd856442fe33a9fe23cbef
DaniAm94 <dani.amico94@gmail.com>
2024-11-23 12:16:55 +0100

    updated reveal and flag cell methods

--- controllers/cells.js
+++ controllers/cells.js
@@ -1,8 +1,51 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const errorHandlerFunction = require("../utils/errorHandlerFunction.js");
+
 module.exports = {
+
+    // Metodo che setta l'attributo revealed di una cell a true
     reveal: async (req, res) => {
-        res.status(200).json("Sono reveal cells")
+
+        const id = parseInt(req.params.cellId);
+
+        try {
+
+            await prisma.cell.update({
+                where: {
+                    id
+                },
+                data: {
+                    revealed: true
+                }
+            })
+
+            res.status(200).json("Cella rivelata con successo")
+        } catch (err) {
+            errorHandlerFunction(res, err)
+        }
     },
+
+    // Metodo che toggola l'attributo flagged di una cell
     flag: async (req, res) => {
-        res.status(200).json("Sono flag cells")
+
+        const id = parseInt(req.params.cellId);
+
+        try {
+
+
+            // executeRaw permette di scrivere una query in SQL raw
+            await prisma.$executeRaw`
+                UPDATE Cell
+                SET flagged = NOT flagged
+                WHERE id = ${id}
+                `;
+
+            res.status(200).json("Cella flaggata con successo")
+        } catch (err) {
+            errorHandlerFunction(res, err);
+        }
+
     }
 }
