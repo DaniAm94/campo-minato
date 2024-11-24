7705e583fdb63c4726d30234bfffc8fc9a3b04c3
DaniAm94 <dani.amico94@gmail.com>
2024-11-24 01:16:14 +0100

    added middleware to check games' status into games' router

--- routers/games.js
+++ routers/games.js
@@ -15,6 +15,10 @@ const validator = require("../middlewares/validator.js");
 // Regole di validazione
 const { paramsId, bodyDifficulty, bodyStatus } = require("../validations/games.js");
 
+// Middleware controllo status
+const checkGameStatus = require("../middlewares/checkGameStatus.js");
+
+
 // Rotte
 
 router.get('/', games.index); // lista partite
@@ -23,17 +27,17 @@ router.post('/', validator(bodyDifficulty), games.store);  // crea una partita
 
 router.get('/:gameId', validator(paramsId), games.show); // restituisce i dettagli di una partita
 
-router.patch('/:gameId', [validator(paramsId), validator(bodyStatus)], games.updateStatus); // aggiorna lo status di una partita
-
 router.delete('/:gameId', validator(paramsId), games.destroy) // cancella una partita
 
-router.patch('/:gameId/pause', validator(paramsId), games.pause); // mette in pausa una partita
+router.patch('/:gameId', [validator(paramsId), validator(bodyStatus), checkGameStatus], games.end); // aggiorna lo status di una partita
 
-router.get('/:gameId/resume', validator(paramsId), games.resume); // riprende una partita in corso
+router.patch('/:gameId/pause', [validator(paramsId), checkGameStatus], games.pause); // mette in pausa una partita
 
-router.patch('/:gameId/restart', validator(paramsId), games.restart); // riavvia una partita in corso
+router.get('/:gameId/resume', [validator(paramsId), checkGameStatus], games.resume); // riprende una partita in corso
 
-router.use('/:gameId/cells', validator(paramsId), cells) // rotte relative alle celle
+router.patch('/:gameId/restart', [validator(paramsId), checkGameStatus], games.restart); // riavvia una partita in corso
+
+router.use('/:gameId/cells', [validator(paramsId), checkGameStatus], cells) // rotte relative alle celle
 
 
 
